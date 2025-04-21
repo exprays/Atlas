eae6c1baefb29ef4b10afaec119fe5e9346b83b0
Surya <suryakant.subudhi@outlook.com>
2025-04-21 13:09:05 +0530

    fix: removed GDAL

--- backend/test.py
+++ backend/test.py
@@ -3,7 +3,6 @@ import rasterio
 import geopandas
 import pyproj
 
-print(gdal.__version__)
 print(f"Rasterio version: {rasterio.__version__}")
 print(f"GeoPandas version: {geopandas.__version__}")
 print(f"PyProj version: {pyproj.__version__}")
