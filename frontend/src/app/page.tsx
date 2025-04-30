714cc2e51df898baa73349053dc937f0ed99b5dd
surya <suryakant.subudhi@outlook.com>
2025-04-30 12:27:13 +0530

    hii

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+//hiii
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 text-white">
